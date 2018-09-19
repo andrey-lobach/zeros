98ab304e53816c2757ce34616327b7b06432aef8
Andrei Lobach <andreylobach1997@gmail.com>
2018-09-19 20:06:51 +0300

    final commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let result = 0;
+  while (number >= 5){
+    result += Math.trunc(number / 5);
+    number = Math.trunc(number/5);
+  }
+    return result;
 }
